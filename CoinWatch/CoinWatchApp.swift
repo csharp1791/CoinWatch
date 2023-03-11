d90be147a3b98d436b83ca85132b7f83eaad4397
Clifford Sharp <csharpusa@gmail.com>
2023-03-11 12:21:00 -0600

    Initial Commit

---
+++ CoinWatch/CoinWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinWatchApp.swift
+//  CoinWatch
+//
+//  Created by Cliff Sharp on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
